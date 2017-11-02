f9134e0be5b8f00fa6eb307062c2b8a075760f59
biao.ma@ninebot.com <jascal@163.com>
2017-11-02 16:10:49 +0800

    update gradle

--- app/src/main/java/com/segway/httputils/MainActivity.java
+++ app/src/main/java/com/segway/httputils/MainActivity.java
@@ -3,6 +3,7 @@ package com.segway.httputils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.segway.httputils.httpclient.HttpClientUtils;
+import com.segway.httputils.httpurlconnection.HttpURLConnectionUtils;
 import com.segway.httputils.okhttp.OKHttpUtils;
 import com.squareup.okhttp.OkHttpClient;
 
@@ -13,11 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        HttpURLConnectionUtils.get();
-//        HttpURLConnectionUtils.post();
-//        HttpClientUtils.get();
-//        HttpClientUtils.post();
-//        OKHttpUtils.get();
+        HttpURLConnectionUtils.get();
+        HttpURLConnectionUtils.post();
+        HttpClientUtils.get();
+        HttpClientUtils.post();
+        OKHttpUtils.get();
         OKHttpUtils.post();
+
     }
 }
